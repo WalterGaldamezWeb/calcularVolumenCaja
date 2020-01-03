6b5d4cc46e9c4c4ddfd185279150a4962fd86e64
WalterGaldamezWeb <walter.galdamez.06@gmail.com>
2020-01-03 11:49:54 -0600

    Calcular volumen de una caja

---
+++ src/calcularvolumencaja/CalcularVolumenCaja.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calcularvolumencaja;
+import java.util.Scanner;
+/**
+ *
+ * @author Walter Gdmz
+ */
+class Caja{
+    double ancho;
+    double alto;
+    double largo;
+    
+    Caja(){
+    }
+    
+    Caja(double ancho,double alto,double profundo){
+        this.ancho=ancho;
+        this.alto=alto;
+        this.largo=largo;
+    }
+    
+    double calcularVolumen(double ancho,double alto,double profundo){
+        double volumen=ancho*alto*profundo;
+        return volumen;
+        }
+}
+public class CalcularVolumenCaja {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Caja objeto = new Caja();
+        
+        Scanner leer = new Scanner(System.in);
+        System.out.println("Ingrese el ancho de la caja");
+        double ancho = leer.nextDouble();
+        System.out.println("Ingrese la altura de la caja");
+        double alto = leer.nextDouble();
+        System.out.println("Ingrese la profundidad de la caja");
+        double largo = leer.nextDouble();
+        
+        double resultado = objeto.calcularVolumen(ancho, alto, largo);
+        
+        System.out.println("El Volumen de la caja es: "+resultado+" cm3");
+    }
+    
+}
